73cf3e1c994275b9b7f7378ddaa258c8192a815a
AlexanderFalkenberg <35661687+AlexanderFalkenberg@users.noreply.github.com>
2022-02-20 12:50:21 +0100

    add: title, meta content

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,65 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+	// Target: https://go.nuxtjs.dev/config-target
+	target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: "marco",
-    htmlAttrs: {
-      lang: "en",
-    },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Personal Training im Kreis Paderborn. Ich helfe Menschen sich in ihrem Körper wohlzufühlen." },
-      { name: "format-detection", content: "telephone=no" },
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-      },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap",
-      },
-    ],
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title:
+			'Marco Meiwes Personal Trainer im Kreis Paderborn',
+		htmlAttrs: {
+			lang: 'en',
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{
+				hid: 'description',
+				name: 'description',
+				content:
+					'Als Personal Trainer im Kreis Paderborn, helfe ich Menschen sich in ihrem Körper wohlzufühlen.',
+			},
+			{ name: 'format-detection', content: 'telephone=no' },
+		],
+		link: [
+			{
+				rel: 'icon',
+				type: 'image/x-icon',
+				href: '/favicon.ico',
+			},
+			{
+				rel: 'preconnect',
+				href: 'https://fonts.gstatic.com',
+			},
+			{
+				rel: 'stylesheet',
+				href: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap',
+			},
+		],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@/assets/css/main.css"],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: ['@/assets/css/main.css'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss",
-  ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/tailwindcss
+		'@nuxtjs/tailwindcss',
+	],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ["@nuxt/content", '@nuxtjs/sitemap'],  
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: ['@nuxt/content', '@nuxtjs/sitemap'],
 
-  sitemap: {hostname:"https://marcomeiwes.de"},
+	sitemap: { hostname: 'https://marcomeiwes.de' },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
 };
